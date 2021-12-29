aaa1c69f6b736e677f70723508791a7d5eb4dafd
zhafran_saloom <52592639+zhafranGit@users.noreply.github.com>
2021-12-29 12:13:44 +0800

    Add files via upload
    week 1

---
+++ W1_D1/day1.js
@@ -0,0 +1,39 @@
+// let height = 165.7;
+// height += 5;
+// console.log(height);
+
+// let phoneNumber = "(081) 23456789";
+
+// const PI = 3.14;
+// PI = 15;
+// console.log(PI);
+
+// var name = "kocheng";
+// let name = "ucing";
+// console.log(name);
+
+// var angka = 18;
+// angka = 17;
+// angka = 20;
+// console.log(angka);
+
+// let name = "Budi";
+// name = "Andi";
+// name = "Indah";
+// console.log(name);
+
+var menu = "Ayam goreng";
+var menu = "Ayam Kecap";
+console.log(menu);
+
+let nama = "rossi";
+let nama = "vaalentino"
+console.log(nama);
+// console.log("5" == 5);
+
+// console.log("123" === 123);
+
+
+// console.log(1 + "1");
+// console.log(100 + "101");
+// console.log("Nama" + 15);
